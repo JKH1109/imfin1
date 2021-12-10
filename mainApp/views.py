11d908528fd11f81d0a4241f1adf8e5f76b4623a
diosong529 <you@example.com>
2021-12-10 15:40:51 +0900

    mainApp view register_done 페이지 연결수정

--- mainApp/views.py
+++ mainApp/views.py
@@ -16,7 +16,7 @@ def mainPage(request):
 class UserCreateView(CreateView):
     template_name='registration/register.html'
     form_class= UserCreationForm
-    success_url = reverse_lazy('register_done')
+    success_url = reverse_lazy('mainApp:register_done')
 
 class UserCreateDoneTV(TemplateView):
     template_name = 'registration/register_done.html'
